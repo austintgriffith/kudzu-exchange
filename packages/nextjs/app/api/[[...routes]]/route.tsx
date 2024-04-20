0843d32c4e35d6efbfc87474e142dc6244628c5d
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-20 09:39:39 +0800

    adding og image

--- packages/nextjs/app/api/[[...routes]]/route.tsx
+++ packages/nextjs/app/api/[[...routes]]/route.tsx
@@ -3,10 +3,13 @@ import { Button, Frog, TextInput } from "frog";
 import { devtools } from "frog/dev";
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
+import { base } from "viem/chains";
 import deployedContracts from "~~/contracts/deployedContracts";
 
-const abi = deployedContracts[8453].BasedKudzuContainer.abi;
-const contractAddress = deployedContracts[8453].BasedKudzuContainer.address;
+const abi = deployedContracts[base.id].BasedKudzuContainer.abi;
+const contractAddress = deployedContracts[base.id].BasedKudzuContainer.address;
+
+const baseUrl = process.env.VERCEL_URL ? `https://kudzu.exchange` : `http://localhost:${process.env.PORT || 3000}`;
 
 const app = new Frog({
   basePath: "/api",
@@ -15,7 +18,7 @@ const app = new Frog({
 app.frame("/", c => {
   return c.res({
     action: "/finish",
-    image: <div style={{ color: "white", display: "flex", fontSize: 60 }}>Perform a transaction</div>,
+    image: `${baseUrl}/kudzu-frame-initial-og.png`,
     intents: [
       <TextInput key="0xYourAddress" placeholder="0xAddressToInfect" />,
       <Button.Transaction key="infect" target="/infect">
@@ -28,7 +31,26 @@ app.frame("/", c => {
 app.frame("/finish", c => {
   const { transactionId } = c;
   return c.res({
-    image: <div style={{ color: "white", display: "flex", fontSize: 60 }}>Transaction ID: {transactionId}</div>,
+    image: (
+      <div
+        style={{
+          backgroundColor: "#0A0A0B",
+          height: "100%",
+          width: "100%",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          gap: 12,
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: 40,
+          paddingTop: 24,
+        }}
+      >
+        <div>Infection Successful!</div>
+        {transactionId}
+      </div>
+    ),
   });
 });
 
@@ -38,7 +60,7 @@ app.transaction("/infect", c => {
   // Contract transaction response.
   return c.contract({
     abi,
-    chainId: "eip155:8453",
+    chainId: `eip155:${base.id}`,
     functionName: "publiclyInfect",
     args: [addressToInfect],
     to: contractAddress,
