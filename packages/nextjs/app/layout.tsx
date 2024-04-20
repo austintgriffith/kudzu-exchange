bebfcef8a5e9eaa4cfc5b7d2dbc45e20a1981f4d
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-20 09:54:21 +0800

    adding generate metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,19 +1,19 @@
 import "@rainbow-me/rainbowkit/styles.css";
+import { getFrameMetadata } from "frog/next";
 import { Metadata } from "next";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const imageUrl = `${baseUrl}/thumbnail.jpg`;
+const baseUrl = process.env.VERCEL_URL ? `https://kudzu.exchange` : `http://localhost:${process.env.PORT || 3000}`;
+
+const imageUrl = `${baseUrl}/kudzu-frame-initial-og.png`;
 
 const title = "Based Kudzu Exchange";
 const titleTemplate = "%s | Based Kudzu Exchange";
 const description = "Built with 🏗 Scaffold-Base";
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
     default: title,
@@ -45,6 +45,14 @@ export const metadata: Metadata = {
     icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
   },
 };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const frameMetadata = await getFrameMetadata(`${baseUrl}/api`);
+  return {
+    ...metadata,
+    other: frameMetadata,
+  };
+}
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
