ad61e7927ed770f9b0bf9eb6b6ad509f78301ec8
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-21 23:50:38 +0800

    try getting infected to address from the event logs

--- packages/nextjs/app/api/[[...routes]]/route.tsx
+++ packages/nextjs/app/api/[[...routes]]/route.tsx
@@ -5,21 +5,20 @@ import { Button, Frog, TextInput } from "frog";
 import { devtools } from "frog/dev";
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
-// import { createPublicClient, http } from "viem";
+import { createPublicClient, decodeEventLog, http } from "viem";
 import { base } from "viem/chains";
 import deployedContracts from "~~/contracts/deployedContracts";
+import externalContracts from "~~/contracts/externalContracts";
 
-// import externalContracts from "~~/contracts/externalContracts";
-
-// const publicClient = createPublicClient({
-//   chain: base,
-//   transport: http(),
-// });
+const publicClient = createPublicClient({
+  chain: base,
+  transport: http(),
+});
 
 const abi = deployedContracts[base.id].BasedKudzuContainer.abi;
-// const kudzuAbi = externalContracts[base.id].KUDZU.abi;
+const kudzuAbi = externalContracts[base.id].KUDZU.abi;
 const contractAddress = deployedContracts[base.id].BasedKudzuContainer.address;
-// const kudzyContractAddress = externalContracts[base.id].KUDZU.address;
+const kudzyContractAddress = externalContracts[base.id].KUDZU.address;
 
 const baseUrl = process.env.VERCEL_URL ? `https://kudzu.exchange` : `http://localhost:${process.env.PORT || 3000}`;
 
@@ -38,65 +37,76 @@ app.frame("/", c => {
   });
 });
 
-// async function getTokenId(address: string) {
-//   try {
-//     const tokenId = await publicClient.readContract({
-//       address: kudzyContractAddress,
-//       abi: kudzuAbi,
-//       functionName: "tokenOfOwnerByIndex",
-//       args: [address, 0n],
-//     });
+async function getTokenId(address: string) {
+  try {
+    const tokenId = await publicClient.readContract({
+      address: kudzyContractAddress,
+      abi: kudzuAbi,
+      functionName: "tokenOfOwnerByIndex",
+      args: [address, 0n],
+    });
 
-//     return tokenId.toString();
-//   } catch (error) {
-//     console.log("error", error);
-//   }
-// }
+    return tokenId.toString();
+  } catch (error) {
+    console.log("error", error);
+  }
+}
 
 app.frame("/finish", async c => {
   const { transactionId } = c;
   const blockExplorerUrl = `${base.blockExplorers.default.url}/tx/${transactionId}`;
 
-  // if (transactionId) {
-  //   const transaction = await publicClient.waitForTransactionReceipt({
-  //     hash: transactionId,
-  //   });
+  if (transactionId) {
+    const transaction = await publicClient.waitForTransactionReceipt({
+      hash: transactionId,
+    });
 
-  //   if (transaction.status === "success") {
-  //     const address = transaction.from;
-  //     const tokenId = await getTokenId(address);
+    if (transaction.status === "success") {
+      // Getting Infect To Address from Event Logs
+      const topics = decodeEventLog({
+        abi: kudzuAbi,
+        data: "0x",
+        topics: transaction.logs[3].topics as [],
+      });
 
-  //     const imageUrl = `https://virus.folia.app/img/base/${tokenId}`;
-  //     const openSeaUrl = `https://opensea.io/assets/base/0x94e84f2dbb9b068ea01db531e7343ec2385b7052/${tokenId}`;
+      let toAddress = transaction.from;
+      if (topics.eventName === "Infect") {
+        toAddress = topics.args.to as `0x${string}`;
+      }
 
-  //     return c.res({
-  //       image: (
-  //         <div
-  //           style={{
-  //             backgroundColor: "#0A0A0B",
-  //             height: "100%",
-  //             width: "100%",
-  //             color: "white",
-  //             display: "flex",
-  //             flexDirection: "column",
-  //             gap: 24,
-  //             justifyContent: "center",
-  //             alignItems: "center",
-  //             fontSize: 50,
-  //             paddingTop: 24,
-  //           }}
-  //         >
-  //           <div>Infection Successful</div>
-  //           {tokenId && <img alt="virus" src={imageUrl} width={400} height={400} />}
-  //         </div>
-  //       ),
-  //       intents: [
-  //         <Button.Link href={openSeaUrl}>View On OpenSea</Button.Link>,
-  //         <Button.Link href={blockExplorerUrl}>View Transaction</Button.Link>,
-  //       ],
-  //     });
-  //   }
-  // }
+      const tokenId = await getTokenId(toAddress);
+
+      const imageUrl = `https://virus.folia.app/img/base/${tokenId}`;
+      const openSeaUrl = `https://opensea.io/assets/base/0x94e84f2dbb9b068ea01db531e7343ec2385b7052/${tokenId}`;
+
+      return c.res({
+        image: (
+          <div
+            style={{
+              backgroundColor: "#0A0A0B",
+              height: "100%",
+              width: "100%",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              gap: 24,
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: 50,
+              paddingTop: 24,
+            }}
+          >
+            <div>Infection Successful</div>
+            {tokenId && <img alt="virus" src={imageUrl} width={400} height={400} />}
+          </div>
+        ),
+        intents: [
+          <Button.Link href={openSeaUrl}>View On OpenSea</Button.Link>,
+          <Button.Link href={blockExplorerUrl}>View Transaction</Button.Link>,
+        ],
+      });
+    }
+  }
 
   return c.res({
     image: (
